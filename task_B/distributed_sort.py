111371094fb9f582ac4bd31e0b39da9d1b0902be
Adam <adamauer1@gmail.com>
2025-06-04 21:00:55 +0200

    update

--- task_B/distributed_sort.py
+++ task_B/distributed_sort.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 import sys
+import heapq
 #sys.setrecursionlimit(10000)
 
 
@@ -15,32 +16,32 @@ def quick_sort(arr):
     right = [x for x in arr if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
 
-def merge_sorted_arrays(arrays):
-    if not arrays:
-        return []
-    if len(arrays) == 1:
-        return arrays[0]
+# def merge_sorted_arrays(arrays):
+#     if not arrays:
+#         return []
+#     if len(arrays) == 1:
+#         return arrays[0]
     
-    mid = len(arrays) // 2
-    left = merge_sorted_arrays(arrays[:mid])
-    right = merge_sorted_arrays(arrays[mid:])
+#     mid = len(arrays) // 2
+#     left = merge_sorted_arrays(arrays[:mid])
+#     right = merge_sorted_arrays(arrays[mid:])
     
-    return merge_two_sorted_arrays(left, right)
+#     return merge_two_sorted_arrays(left, right)
 
-# extend to work with n number of arrays
-def merge_two_sorted_arrays(left, right):
-    result = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
+# # extend to work with n number of arrays
+# def merge_two_sorted_arrays(left, right):
+#     result = []
+#     i = j = 0
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             result.append(left[i])
+#             i += 1
+#         else:
+#             result.append(right[j])
+#             j += 1
+#     result.extend(left[i:])
+#     result.extend(right[j:])
+#     return result
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -49,7 +50,7 @@ size = comm.Get_size()
 chunks = None
 
 if rank == 0:
-    data = pd.read_csv("random_data_1000.csv", header=None).values.flatten()[1:]
+    data = pd.read_csv("random_data_50.csv", header=None).values.flatten()[1:]
     chunks = np.array_split(data, size)
 
 total_compute_time = MPI.Wtime()
@@ -70,13 +71,15 @@ gather_time = MPI.Wtime() - gather_time
 if rank == 0:
     final_merge_time = MPI.Wtime()
     #sorted_data = quick_sort(np.concatenate(sorted_chunks))
-    sorted_data = merge_sorted_arrays(sorted_chunks)
+    #sorted_data = merge_sorted_arrays(sorted_chunks)
+    sorted_data = list(heapq.merge(*sorted_chunks))
     final_merge_time = MPI.Wtime() - final_merge_time
     total_compute_time = MPI.Wtime() - total_compute_time
-    standard_sort_time = MPI.Wtime()
-    data = quick_sort(data)
-    standard_sort_time = MPI.Wtime() - standard_sort_time
-    print(f"Standard sort time: {standard_sort_time} seconds.")
+    #print(sorted_data)
+    #@standard_sort_time = MPI.Wtime()
+    #data = quick_sort(data)
+    #standard_sort_time = MPI.Wtime() - standard_sort_time
+    #print(f"Standard sort time: {standard_sort_time} seconds.")
     print(f"Process {rank} sorted data in {compute_time} seconds.")
     print(f"Scatter time: {scatter_time} seconds.")
     print(f"Gather time: {gather_time} seconds.")
